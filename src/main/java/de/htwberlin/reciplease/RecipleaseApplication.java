3d2d5f26d5015102b9ed7db715a1645b82584fe2
Ercan Kurt <ercan257@outlook.de>
2023-10-18 16:21:18 +0200

    Initialer Commit

---
+++ src/main/java/de/htwberlin/reciplease/RecipleaseApplication.java
@@ -0,0 +1,13 @@
+package de.htwberlin.reciplease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipleaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipleaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htwberlin/reciplease/RecipleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.reciplease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipleaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
