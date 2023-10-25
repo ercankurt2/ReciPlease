bdf35c4b15fd6c238bced0f4ae8a93bf60795884
Ercan Kurt <ercan257@outlook.de>
2023-10-25 13:57:27 +0200

    Bugfix Übung Parameterübergabe

--- src/main/java/de/htwberlin/reciplease/Controller.java
+++ src/main/java/de/htwberlin/reciplease/Controller.java
@@ -5,51 +5,37 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class Controller {
 
+    // Variante 1 (als Pfadparameter in der URL)
     @GetMapping("/")
-    public String index() {
-        return "Willkommen bei ReciPlease!";
+    public String willkommen1() {
+        return "Willkommen bei ReciPlease";
+    }
+
+    @GetMapping("/user")
+    public String willkommen1_v1() {
+        return "Willkommen bei ReciPlease";
+    }
+
+    @GetMapping("/user/{userid}")
+    public String willkommen1_v2(@PathVariable int userid) {
+        return "Willkommen bei ReciPlease, User " + userid + "!";
     }
 
     @GetMapping("/user/{userid}/password/{password}")
-    public String index2(@PathVariable int userid, @PathVariable String password) {
-
-        return "Willkommen bei ReciPlease!, user " + userid + ", your password is " + password + "!" ;
-
+    public String willkommen1_v3(@PathVariable int userid, @PathVariable String password) {
+        return "Willkommen bei ReciPlease, User " + userid + ", dein Passwort ist " + password + "!" ;
     }
-    //localhost:8080/user?userid=123&password=test
-    //localhost:8080/user?password=test&userid=123&einweitererparam=blabla
-    @GetMapping("/user")
-    public String index3(@RequestParam int userid, @RequestParam String password) {
-        return "Willkommen bei ReciPlease!, user" + userid + ", your password is " + password + "!";
-   //// Variante 1 (als Pfadparameter in der URL)
-        //    @GetMapping("/")
-        //    public String bla() {
-        //        return "Greetings from Webtech";
-        //    }
-        //
-        //    @GetMapping("/user")
-        //    public String bla2() {
-        //        return "Greetings from Webtech";
-        //    }
-        //
-        //    @GetMapping("/user/{userid}")
-        //    public String bla3(@PathVariable int userid) {
-        //        return "Greetings from Webtech, user " + userid + "!";
-        //    }
-        //
-        //    @GetMapping("/user/{userid}/pw/{password}")
-        //    public String bla4(@PathVariable int userid, @PathVariable String password) {
-        //        return "Greetings from Webtech, user " + userid + ", your password is " + password + "!";
-        //    }
-        //
-        //    // Variante 2 (als Queryparameter in der URL)
-        //    @GetMapping("/user")
-        //    public String bla5(@RequestParam int userid, @RequestParam String password) {
-        //        return "Greetings from Webtech, user " + userid + ", your password is " + password + "!";
-        //    }
+
+    // Variante 2 (als Queryparameter in der URL)
+    // localhost:8080/user2?userid=123&password=test
+    // localhost:8080/user2?password=test&userid=123&einweitererparam=blabla
+    @GetMapping("/user2")
+    public String willkommen2(@RequestParam int userid, @RequestParam String password) {
+        return "Willkommen bei ReciPlease!, User" + userid + ", dein Passwort ist " + password + "!";
     }
 
 }
