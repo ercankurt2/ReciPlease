decc49ac2888b6e559f98a843aad03bae9f0cbb8
Ercan Kurt <ercan257@outlook.de>
2023-11-01 19:12:00 +0100

    Annotationen hinzugefügt

--- src/main/java/de/htwberlin/reciplease/RecipleaseApplication.java
+++ src/main/java/de/htwberlin/reciplease/RecipleaseApplication.java
@@ -2,8 +2,12 @@ package de.htwberlin.reciplease;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
+@EnableJpaRepositories
 public class RecipleaseApplication {
 
 	public static void main(String[] args) {
