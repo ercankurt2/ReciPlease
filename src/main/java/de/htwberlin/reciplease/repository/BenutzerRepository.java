4493c20a4272127fcbf033e4f64630e509097115
Ercan Kurt <ercan257@outlook.de>
2023-11-27 19:11:54 +0100

    Implementierung BenutzerService

--- src/main/java/de/htwberlin/reciplease/repository/BenutzerRepository.java
+++ src/main/java/de/htwberlin/reciplease/repository/BenutzerRepository.java
@@ -4,9 +4,12 @@ import de.htwberlin.reciplease.model.Benutzer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 // Generisches Interface mit Benutzer und ID (Integer)
 // @Repository-Annotation kennzeichnet dieses Interface als Spring Data JPA-Repository
 @Repository
 public interface BenutzerRepository extends JpaRepository<Benutzer, Integer> {
-
+    // Benutzerdefinierte Abfrage, um Benutzer nach Benutzernamen als String zu durchsuchen
+    List<Benutzer> findBenutzerByBenutzernameContaining(String benutzername);
 }
--- src/main/java/de/htwberlin/reciplease/service/BenutzerService.java
+++ src/main/java/de/htwberlin/reciplease/service/BenutzerService.java
@@ -1,8 +1,11 @@
 package de.htwberlin.reciplease.service;
 
+import de.htwberlin.reciplease.model.Benutzer;
 import de.htwberlin.reciplease.repository.BenutzerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 // @Service-Annotation kennzeichnet diese Klasse als einen Spring-Service,
 // der die Geschäftslogik der Anwendung implementiert
@@ -19,5 +22,28 @@ public class BenutzerService {
         Integer count = Math.toIntExact(benutzerRepository.count());
         // Erhöht die Anzahl der Benutzer um 1, um eine eindeutige BenutzerID zu generieren
         return count + 1;
+    }
+
+    // Methode zur Erstellung eines neuen Benutzers in der Datenbank
+    public Benutzer createBenutzer(Benutzer benutzer) {
+        // Setzen der eindeutigen BenutzerID
+        benutzer.setBenutzerID(generateBenutzerID());
+
+        // Speichert den übergebenen Benutzer
+        benutzerRepository.save(benutzer);
+
+        // Gibt den erstellten Benutzer zurück
+        return benutzer;
+    }
+
+    // Methode zur Abfrage aller Benutzer in der Datenbank
+    public List<Benutzer> getAllBenutzer() {
+        return benutzerRepository.findAll(); // Ruft alle Benutzer ab
+    }
+
+    // Methode zur Suche eines Benutzers anhand des Namens
+    public List<Benutzer> searchByName(String benutzername) {
+        // Ruft eine benutzerdefinierte Suche nach Benutzern anhand des Benutzernamens ab
+        return benutzerRepository.findBenutzerByBenutzernameContaining(benutzername);
     }
 }
