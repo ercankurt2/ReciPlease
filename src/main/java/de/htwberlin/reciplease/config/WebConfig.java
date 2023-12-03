bae80ef42c8fb72aef050f318449e2032ab8c4c9
Ercan Kurt <ercan257@outlook.de>
2023-12-03 12:46:11 +0100

    Web-Konfiguration

---
+++ src/main/java/de/htwberlin/reciplease/config/WebConfig.java
@@ -0,0 +1,20 @@
+package de.htwberlin.reciplease.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedMethods("*")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://reciplease1-fe-e3c474f21ef1.herokuapp.com/"
+                );
+    }
+}
