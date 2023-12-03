6dcbdb6cde472a42ecc15f080209694452d19540
MeteHan98 <s0584169@htw-berlin.de>
2023-12-03 21:51:45 +0100

    Implementierung KategorieController.java

--- src/main/java/de/htwberlin/reciplease/controller/KategorieController.java
+++ src/main/java/de/htwberlin/reciplease/controller/KategorieController.java
@@ -1,6 +1,7 @@
 package de.htwberlin.reciplease.controller;
 
 import de.htwberlin.reciplease.model.Kategorie;
+import de.htwberlin.reciplease.model.Rezept;
 import de.htwberlin.reciplease.service.KategorieService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -26,4 +27,36 @@ public class KategorieController {
     public ResponseEntity<List<Kategorie>> fetchKategorien() {
         return ResponseEntity.ok(kategorieService.getAllKategorie());
     }
+
+    // HTTP-GET, um eine Kategorie anhand ihrer ID zu erhalten
+    @GetMapping("/{id}") // Der Pfad enthält eine Variable für die ID
+    public ResponseEntity<Kategorie> getKategorieById(@PathVariable Integer id) {
+        Kategorie kategorie = kategorieService.getKategorieById(id);
+        if (kategorie != null) {
+            return ResponseEntity.ok(kategorie);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{id}") // HTTP-PUT, um eine Kategorie anhand ihrer ID zu aktualisieren
+    public ResponseEntity<Kategorie> updateKategorie(@PathVariable Integer id, @RequestBody Kategorie kategorie) {
+        Kategorie updatedKategorie = Kategorie.updateKategorie(id, kategorie);
+        if (updatedKategorie != null) {
+            return ResponseEntity.ok(updatedKategorie);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}") // HTTP-DELETE, um eine Kategorie anhand seiner ID zu löschen
+    public ResponseEntity<Kategorie> deleteKategorie(@PathVariable Integer id) {
+        Kategorie kategorie = kategorieService.deleteKategorie(id);
+        if (kategorie != null) {
+            return ResponseEntity.ok(kategorie);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
+
