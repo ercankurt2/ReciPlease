f8302a416375783e15f43957a3ac825c4f9ad87b
MeteHan98 <s0584169@htw-berlin.de>
2023-12-03 22:34:03 +0100

    Implementierung ZutatService.java

--- src/main/java/de/htwberlin/reciplease/service/ZutatService.java
+++ src/main/java/de/htwberlin/reciplease/service/ZutatService.java
@@ -1,5 +1,6 @@
 package de.htwberlin.reciplease.service;
 
+import de.htwberlin.reciplease.model.Rezept;
 import de.htwberlin.reciplease.model.Zutat;
 import de.htwberlin.reciplease.repository.ZutatRepository;
 import lombok.RequiredArgsConstructor;
@@ -36,7 +37,52 @@ public class ZutatService {
         // Gibt die erstellte Zutat zurück
         return zutat;
     }
+    // Methode zur Aktualisierung von Zutat in der Datenbank
+    public Zutat updateZutat(Integer id, Zutat zutatDetails) {
+        // Ruft die Zutat anhand ihrer ID aus der Datenbank ab
+        Zutat zutatToUpdate = zutatRepository.findById(id).orElse(null);
 
+        if (zutatToUpdate != null) {
+            // Aktualisieren der Daten einer Zutat mit den Details aus dem übergebenen Zutat-Objekt
+            zutatToUpdate.setName(zutatDetails.getName());
+            zutatToUpdate.setMenge(zutatDetails.getMenge());
+            zutatToUpdate.setEinheit(zutatDetails.getEinheit());
+
+            // Speichern der aktualisierten Zutat in der Datenbank
+            zutatRepository.save(zutatToUpdate);
+
+            // Rückgabe der aktualisierten Zutat
+            return zutatToUpdate;
+        } else {
+            // Wenn die Zutat nicht gefunden wird, wird eine geeignete Ausnahme geworfen oder null zurückgegeben
+            // Option: eine benutzerdefinierte Ausnahme werfen
+            // throw new EntityNotFoundException("Zutat mit ID " + id + " nicht gefunden.");
+
+            // Option: null zurückgeben
+            return null;
+        }
+    }
+
+    // Methode zur Löschung einer Zutat aus der Datenbank
+    public Zutat deleteZutat(Integer id) {
+        // Ruft die Zutat anhand ihrer ID aus der Datenbank ab
+        Zutat zutatToDelete = zutatRepository.findById(id).orElse(null);
+
+        if (zutatToDelete != null) {
+            // Löscht die Zutat aus der Datenbank
+            zutatRepository.delete(zutatToDelete);
+
+            // Rückgabe des gelöschten Rezepts
+            return zutatToDelete;
+        } else {
+            // Wenn die Zutat nicht gefunden wird, wird eine geeignete Ausnahme geworfen oder null zurückgegeben
+            // Option: eine benutzerdefinierte Ausnahme werfen
+            // throw new EntityNotFoundException("Zutat mit ID " + id + " nicht gefunden.");
+
+            // Option: null zurückgeben
+            return null;
+        }
+    }
     // Methode zur Abfrage aller Zutaten in der Datenbank
     public List<Zutat> getAllZutat() {
         return zutatRepository.findAll(); // Ruft alle Zutaten ab
@@ -46,5 +92,11 @@ public class ZutatService {
     public List<Zutat> searchByName(String zutatname) {
         // Ruft eine benutzerdefinierte Suche nach Zutaten anhand des Zutatnamens ab
         return zutatRepository.findZutatByNameContaining(zutatname);
+    }
+
+    // Methode, um eine Zutat anhand ihrer ID zu erhalten
+    public Zutat getZutatById(Integer id) {
+        // Verwendung von findById, um eine Zutat zu finden oder null zurückzugeben
+        return zutatRepository.findById(id).orElse(null);
     }
 }
