42a9c3bab4cb0b263dbfa20f1073a7a5dabd196e
Ercan Kurt <ercan257@outlook.de>
2024-01-12 22:10:18 +0100

    Update CORS

--- src/main/java/de/htwberlin/reciplease/config/WebConfig.java
+++ src/main/java/de/htwberlin/reciplease/config/WebConfig.java
@@ -10,7 +10,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        // Erlaubt alle HTTP-Methoden und -Header von allen aufgelisteten Quellen
+        registry.addMapping("/**")
                 .allowedMethods("*")
                 .allowedOrigins(
                         "http://localhost:3000",
